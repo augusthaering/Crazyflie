f4a017797e6bea5a5cc19803a4d909dd7a1901ab
augusthaering <138245871+augusthaering@users.noreply.github.com>
2025-03-19 17:38:27 +0100

    s

--- motion_flying.py
+++ motion_flying.py
@@ -1,133 +1,83 @@
-# -*- coding: utf-8 -*-
-#
-#     ||          ____  _ __
-#  +------+      / __ )(_) /_______________ _____  ___
-#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
-#
-#  Copyright (C) 2017 Bitcraze AB
-#
-#  Crazyflie Nano Quadcopter Client
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
 import logging
 import sys
 import time
-from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E705')
-
 DEFAULT_HEIGHT = 0.5
-BOX_LIMIT = 0.5
-
-deck_attached_event = Event()
 
 logging.basicConfig(level=logging.ERROR)
 
-position_estimate = [0, 0]
-
-
-def move_box_limit(scf):
-    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        body_x_cmd = 0.2
-        body_y_cmd = 0.1
-        max_vel = 0.2
-
-        while (1):
-            '''if position_estimate[0] > BOX_LIMIT:
-                mc.start_back()
-            elif position_estimate[0] < -BOX_LIMIT:
-                mc.start_forward()
-            '''
-
-            if position_estimate[0] > BOX_LIMIT:
-                body_x_cmd = -max_vel
-            elif position_estimate[0] < -BOX_LIMIT:
-                body_x_cmd = max_vel
-            if position_estimate[1] > BOX_LIMIT:
-                body_y_cmd = -max_vel
-            elif position_estimate[1] < -BOX_LIMIT:
-                body_y_cmd = max_vel
-
-            mc.start_linear_motion(body_x_cmd, body_y_cmd, 0)
-
-            time.sleep(0.1)
-
-
-def move_linear_simple(scf):
-    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        time.sleep(1)
-        mc.forward(0.5)
-        time.sleep(1)
-        mc.turn_left(180)
-        time.sleep(1)
-        mc.forward(0.5)
-        time.sleep(1)
-
-
-def take_off_simple(scf):
-    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        time.sleep(3)
-        mc.stop()
-
-
-def log_pos_callback(timestamp, data, logconf):
-    print(data)
-    global position_estimate
-    position_estimate[0] = data['stateEstimate.x']
-    position_estimate[1] = data['stateEstimate.y']
-
-
-def param_deck_flow(_, value_str):
-    value = int(value_str)
-    print(value)
-    if value:
-        deck_attached_event.set()
-        print('Deck is attached!')
+def check_lighthouse_system(scf):
+    print("Checking Lighthouse V2 setup...")
+    
+    system_type = scf.cf.param.get_value("lighthouse.systemType")
+    print(f"Lighthouse system type: {system_type}")
+    
+    if int(system_type) == 2:
+        print("✅ Lighthouse V2 detected!")
+        return True
     else:
-        print('Deck is NOT attached!')
+        print("❌ Lighthouse V2 NOT detected!")
+        return False
 
+def stabilize_and_takeoff(scf):
+    """ Initialisiert die Position und führt einen stabilen Takeoff durch """
+    print("Initializing position (0,0,0)...")
+    for _ in range(20):
+        scf.cf.extpos.send_extpos(0.0, 0.0, 0.0)
+        time.sleep(0.1)
+
+    print("Taking off to default height...")
+    for _ in range(40):  # Langsam steigen für Stabilität
+        scf.cf.commander.send_position_setpoint(0.0, 0.0, DEFAULT_HEIGHT, 0.0)
+        time.sleep(0.1)
+
+    print("Hovering for 2 seconds...")
+    time.sleep(2)  # Warten, bis sich die Drohne stabilisiert
+
+def move_stably(scf):
+    """ Führt langsame, kontrollierte Bewegungen aus """
+    print("Moving to absolute position (0.5m, 0.5m)...")
+    for _ in range(50):  # Langsames Bewegen
+        scf.cf.commander.send_position_setpoint(0.5, 0.5, DEFAULT_HEIGHT, 0.0)
+        time.sleep(0.1)
+
+    print("Hovering for 2 seconds...")
+    time.sleep(2)
+
+    print("Returning to start position")
+    for _ in range(50):
+        scf.cf.commander.send_position_setpoint(0.0, 0.0, DEFAULT_HEIGHT, 0.0)
+        time.sleep(0.1)
+
+    print("Hovering for 2 seconds before landing...")
+    time.sleep(2)
+
+def land_safely(scf):
+    """ Führt eine langsame Landung durch """
+    print("Landing...")
+    for _ in range(40):  # Langsame Landung
+        scf.cf.commander.send_position_setpoint(0.0, 0.0, 0.1, 0.0)
+        time.sleep(0.1)
+
+    scf.cf.commander.send_stop_setpoint()
+    print("Landed safely.")
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+        scf.cf.platform.send_arming_request(True)
+        time.sleep(1.0)
 
-        scf.cf.param.add_update_callback(group='deck', name='bcFlow2',
-                                         cb=param_deck_flow)
-        time.sleep(1)
-
-        logconf = LogConfig(name='Position', period_in_ms=10)
-        logconf.add_variable('stateEstimate.x', 'float')
-        logconf.add_variable('stateEstimate.y', 'float')
-        scf.cf.log.add_config(logconf)
-        logconf.data_received_cb.add_callback(log_pos_callback)
-
-        if not deck_attached_event.wait(timeout=5):
-            print('No flow deck detected!')
+        if not check_lighthouse_system(scf):
             sys.exit(1)
 
-        logconf.start()
-
-        take_off_simple(scf)
-        # move_linear_simple(scf)
-        # move_box_limit(scf)
-        logconf.stop()
+        stabilize_and_takeoff(scf)
+        move_stably(scf)
+        land_safely(scf)
